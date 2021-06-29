2b5fbcb0d374c21ab4d552509414d8016658d019
Moon Jaewon <jaewonmoo.keti@gmail.com>
2021-06-29 18:22:59 +0900

    Minor Modification

--- missing_data/small_nan_data_processing.py
+++ missing_data/small_nan_data_processing.py
@@ -1,4 +1,4 @@
-def simple_nan_processing(data, limit_nan_num ): 
+def simple_nan_processing(data, limit_nan_num): 
 
     data_non_nan= data.interpolate(method='values', limit= limit_nan_num)
     return data_non_nan
