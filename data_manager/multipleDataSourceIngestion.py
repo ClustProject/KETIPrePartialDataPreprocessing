a70695bca56a67867aa1375a4d1216dc2b862417
jaewonMoon <jaewonmoon.keti@gmail.com>
2021-10-29 09:58:17 +0900

    minor modification

--- data_manager/multipleDataSourceIngestion.py
+++ data_manager/multipleDataSourceIngestion.py
@@ -14,15 +14,15 @@ class getData():
         dataset = pd.read_csv(file_name, parse_dates=True, index_col=[time_index])
         return dataset
 
-    def getInfluxInput(self):
+    def getInfluxInput(self, db_name, ms_name):
         from KETIPreDataIngestion.KETI_setting import influx_setting_KETI as ins
         from KETIPreDataIngestion.data_influx import influx_Client
        
         ##
         DBClient = influx_Client.influxClient(ins)
-        db_list = DBClient.get_DBList()
+        db_list = DBClient.get_DBList() # 나중에 DB 이름 바꾸고 싶을때 여기 체크 가능
         db_name  = 'air_indoor_경로당'
-        ms_list = DBClient.measurement_list(db_name)
+        ms_list = DBClient.measurement_list(db_name) # MS 이름 바꾸고 싶을때 여기 체크 가능
         ms_name = ms_list[0]
 
         feature_list = DBClient.get_fieldList(db_name, ms_name)
