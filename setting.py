fb406799dd9071852d0246f75471306fb245b658
jwmmoonP <jaewonmoon.keti@gmail.com>
2021-12-22 17:05:35 +0900

    git ignore

--- setting.py
+++ setting.py
@@ -22,8 +22,8 @@ outlier_param  = {
 imputation_param = {
 "serialImputation":{
     "flag":True,
-    "imputation_method":[{"min":0,"max":3,"method":"linear", "parameter":{}}, 
-                            #{"min":4,"max":6,"method":"mean", "parameter":{"model_address":model_folder}}
+    "imputation_method":[{"min":0,"max":3,"method":"KNN", "parameter":{}}, 
+                            #{"min":4,"max":6,"method":"brits", "parameter":{"model_address":model_folder}}
                             {"min":4,"max":6,"method":"mean", "parameter":{}}
     ],"totalNanLimit":20}
 }
