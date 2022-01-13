d768cd6c19a2f028e1db44789712114af1c48c9d
jwmmoonP <jaewonmoon.keti@gmail.com>
2022-01-13 19:00:14 +0900

    make new repository

--- data_imputation/DL/DLImputation.py
+++ data_imputation/DL/DLImputation.py
@@ -8,27 +8,31 @@ class DLImputation():
 
     def getResult(self):
         result = self.data.copy()
+
+        ### Brits
         if self.method == 'brits':
             print("birts_imputation")
             for column_name in self.data.columns:
-               result = self._britsImputation(self.data[[column_name]], self.parameter, column_name)
+               result = britsColumnImputation(self.data[[column_name]], self.parameter, column_name)
                result[column_name] = result
+               
+        ### Define Another Imputation 
         else:
             result = self.data
         return result
 
     
-    def _britsImputation(self, data, parameter, column_name):
-        model_address = parameter['model_address']
-        if os.path.isdir(model_address):
-            from KETIPrePartialDataPreprocessing.data_imputation.DL.brits import inference
-            dataset = [data[[column_name]][i:i+1000] for i in range(0, len(data), 1000)]
-            result = pd.DataFrame()
-            for split_data in dataset:
-                result_split = inference.BritsInference(split_data, model_address, column_name).get_result()
-                result = pd.concat([result, result_split])
-        else:
-            result = data.copy()
-            print("No Brits Folder")
+def britsColumnImputation(data, parameter, column_name):
+    model_address = parameter['model_address']
+    if os.path.isdir(model_address):
+        from KETIToolDL.imputation.brits import inference
+        dataset = [data[[column_name]][i:i+1000] for i in range(0, len(data), 1000)]
+        result = pd.DataFrame()
+        for split_data in dataset:
+            result_split = inference.BritsInference(split_data, model_address, column_name).get_result()
+            result = pd.concat([result, result_split])
+    else:
+        result = data.copy()
+        print("No Brits Folder")
 
-        return result
+    return result
--- data_imputation/DL/DLImputationTraining.py
+++ data_imputation/DL/DLImputationTraining.py
@@ -7,73 +7,56 @@ sys.path.append("../..")
 sys.path.append("../../..")
 sys.path.append("../../../..")
 
-from KETIPrePartialDataPreprocessing.data_imputation.DL.brits import training
+from KETIToolDL.Imputation.brits import training
+from KETIToolDL.InfluxDB import InfluxDBTraining as IDT
 
-class ImputationTraining():
-    def __init__(self, dbClient, rootDir):
-        self.DBClient = dbClient
-        self.root_dir = rootDir
+class BritsInfluxTraining(IDT.InfluxDBTraining):
+    def setModelFileName(self, target_name):
+        self.model_name = target_name + '.pth'
+        self.json_name = target_name  + '.json'
+        self.model_path =os.path.join(self.model_folder, self.model_name)
+        self.json_path = os.path.join(self.model_folder, self.json_name)
 
-    def trainerForDB(self, db_name, bind_params):
-        self.db_name = db_name
-        self.MSList = self.DBClient.measurement_list(self.db_name)
-        for ms_name in self.MSList:
-            self.trainerForMS(db_name, ms_name, bind_params)
+    def trainSaveModel(self, df): 
+        Brits = training.BritsTraining(df, self.json_path)
+        model = Brits.train()
+        torch.save(model.state_dict(), self.model_path)
+        print(self.model_path)
 
-    def trainerForMS(self, db_name, ms_name, bind_params):
-        df = self.DBClient.get_data_by_time(bind_params, db_name, ms_name)
-        for column_name in df.columns:
-            column_data = df[[column_name]]
-            model_name = self.trainSaveModel(column_data, column_name)
-            print(model_name, " saved")
-
-    def trainSaveModel(self, df, df_name): 
-        self.model_folder = os.path.join(self.root_dir, db_name, ms_name) 
-
-        if not os.path.exists(self.model_folder):
-            os.makedirs(self.model_folder) 
-
-        self.model_addr = os.path.join(self.root_dir, db_name, ms_name, df_name)
-        model_name = self.model_addr + '.pth'
-        json_name = self.model_addr  + '.json'
-
-        T = training.britsImputationTraining(df, json_name)
-        model = T.columnDataTrainer()
-        torch.save(model.state_dict(), model_name)
-        return model_name
-         
-        
-
+def trainInfluxData(model_method, mode):
+    if model_method == 'brits':
+        trainer = BritsInfluxTraining(DBClient, rootDir)
+    if mode == 'MS_Training':
+        ## train for Measurment
+        trainer.trainerForMS(db_name, ms_name, bind_params)
+    
+    elif mode == 'DB_Training':
+        ## train for Database
+        trainer.trainerForDB(db_name, bind_params)
 
 if __name__ == '__main__':
     #print(sys.path)
     from KETIPreDataIngestion.KETI_setting import influx_setting_KETI as ins
     from KETIPreDataIngestion.data_influx import influx_Client
-
-    DBClient = influx_Client.influxClient(ins.CLUSTDataServer)
     
-    # Example 1 
-    # db_name = 'air_indoor_경로당'
-    # ms_name = 'ICL1L2000234'
+    ##########################################
+    mode_list = ['MS_Training', 'DB_Training']
+    model_method_list = ['brits']
+    model_purpose_list=['imputation']
+    ##########################################
+    DBClient = influx_Client.influxClient(ins.CLUSTDataServer)
     db_name = 'air_indoor_요양원'
     ms_name = 'ICL1L2000017'
-    first = DBClient.get_first_time(db_name, ms_name)
-    last = DBClient.get_last_time(db_name, ms_name)
-    print(first, last)
-    
+    #first = DBClient.get_first_time(db_name, ms_name)
+    #last = DBClient.get_last_time(db_name, ms_name)
     first ='2020-06-18'
     last ='2020-06-19'
-
-    rootDir = os.path.join(os.getcwd(), 'data_imputation', 'DL', 'brits', 'model')
+    ##########################################
     bind_params = {'end_time':last, 'start_time': first}
-
-    mode_list = ['MS_Training', 'DB_Training']
     mode = mode_list[0] ## mode select
-    imT = ImputationTraining(DBClient, rootDir)
-    if mode == 'MS_Training':
-        ## train for Measurment
-        imT.trainerForMS(db_name, ms_name, bind_params)
-    elif mode == 'DB_Training':
-        ## train for Database
-        imT.trainerForDB(db_name, bind_params)
-    
+    model_purpose = model_purpose_list[0]
+    model_method = model_method_list[0]
+    rootDir = os.path.join('Users', 'bunnyjw','Git', 'DL', 'Models', model_purpose, model_method)
+    ###########################################
+
+    trainInfluxData(model_method, mode)
