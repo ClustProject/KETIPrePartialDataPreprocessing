e25f4b7e116637317453b627f635f6214ce9bf94
keymemroy <keymemory@naver.com>
2022-04-05 17:53:20 +0900

    directory path change

--- dataRemovebyNaN/clean_feature_data.py
+++ dataRemovebyNaN/clean_feature_data.py
@@ -11,6 +11,8 @@ from KETIPrePartialDataPreprocessing import data_preprocessing
 
 class CleanFeatureData:
     def __init__(self, feature_list, freq_min):
+        """
+        """
         self.feature_list = feature_list
         from datetime import timedelta
         self.resample_freq = timedelta(minutes = freq_min) 
@@ -38,7 +40,23 @@ class CleanFeatureData:
         self.query_start_time = query_start_time
         self.query_end_time = query_end_time
 
-    def getMultipleCleanDataSetsByFeature(self, dataSet, NanInfoForClenData) :
+    def getMultipleCleanDataSetsByFeature(self, dataSet, NanInfoForCleanData) :
+        """
+        refinedDataSet, refinedDataSetName: 간단한 cleaning 진행한 데이터셋
+        NaNRemovedDataSet : 품질이 좋지 않은 NaN 값을 다수 포함한 컬럼을 제거한 데이터
+        ImputedDataSet: datasetNanRemove의 nan을 임의대로 interpolation한 데이터
+        ImputedDataSetName: datasetNanRemove 에 대한 ms 이름
+
+
+        :param dataSet: input Data to be handled
+        :type dataSet: dictionary
+        :param NanInfoForCleanData: selection condition
+        :type NanInfoForCleanData: dictionary
+
+        :returns: self.refinedDataSet, self.refinedDataSetName, self.NaNRemovedDataSet, self.ImputedDataSetName, self.ImputedDataSet
+        :rtype: 
+        """
+
         self.refinedDataSet={}
         self.refinedDataSetName={}
         self.NaNRemovedDataSet={}
@@ -55,7 +73,7 @@ class CleanFeatureData:
         for ms_name in ms_list:
             print("=======",ms_name,"=======")
             data = dataSet[ms_name]
-            refinedData, NaNRemovedData, ImputedData, finalFlag  = self.getOneCleanDataSetByFeature(data, NanInfoForClenData)
+            refinedData, NaNRemovedData, ImputedData, finalFlag  = self.getOneCleanDataSetByFeature(data, NanInfoForCleanData)
             for feature in self.feature_list:
                 if feature in data.columns:
                     if finalFlag[feature]==-1:
@@ -67,24 +85,20 @@ class CleanFeatureData:
                             self.NaNRemovedDataSet[feature].append(NaNRemovedData[[feature]])
                             self.ImputedDataSet[feature].append(ImputedData[[feature]])
                             self.ImputedDataSetName[feature].append(ms_name)
-                        
-        """
-        refinedDataSet, refinedDataSetName: 간단한 cleaning 진행한 데이터셋
-        NaNRemovedDataSet : 품질이 좋지 않은 NaN 값을 다수 포함한 컬럼을 제거한 데이터
-        ImputedDataSet: datasetNanRemove의 nan을 임의대로 interpolation한 데이터
-        ImputedDataSetName: datasetNanRemove 에 대한 ms 이름
-        """ 
+                    
         return self.refinedDataSet, self.refinedDataSetName, self.NaNRemovedDataSet, self.ImputedDataSetName, self.ImputedDataSet
 
-    
-    def getOneCleanDataSetByFeature(self, data, NanInfoForClenData) :
+    # getMultipleCleanDataSetsByFeature() - getOneCleanDataSetByFeature() - _getDataWithFullDuration()
+    # 현재 위의 3개가 연결되어 있음
+
+    def getOneCleanDataSetByFeature(self, data, NanInfoForCleanData) :
         """
         This function gets CleanDataSet by Feature
 
         :param data: input Data to be handled
         :type data: dataFrame
-        :param NanInfoForClenData:  selection condition
-        :type NanInfoForClenData: dictionary
+        :param NanInfoForCleanData:  selection condition
+        :type NanInfoForCleanData: dictionary
 
         :returns: refinedData
         :rtype: dataframe
@@ -96,6 +110,7 @@ class CleanFeatureData:
         :rtype: dictionary (-1: no data, 0:useless data, 1:useful data)
         """
 
+        # start_time, end_time 문제 - 기간 설정이 고정되어 있는 이슈
         data = self._getDataWithFullDuration(data)
         refinedData, DataWithMoreNaN = self._getPreprocessedData(data)
 
@@ -103,20 +118,21 @@ class CleanFeatureData:
         NaNRemovedData = {}
         ImputedData = {}
         DRN = data_remove_byNaN.DataRemoveByNaNStatus()
-        NaNRemovedData = DRN.removeNaNData(DataWithMoreNaN, NanInfoForClenData)
+        NaNRemovedData = DRN.removeNaNData(DataWithMoreNaN, NanInfoForCleanData)
         ImputedData = NaNRemovedData.copy()
         
         for feature in self.feature_list:
             finalFlag[feature] = -1
-            if (feature in data.columns) and (len(refinedData) >0):
+            if (feature in data.columns) and (len(refinedData) >0): # refined_data 가 존재하고, 기존 data에 feature(column)이 속해 있을 때
                 finalFlag[feature] = 0
-                if feature in NaNRemovedData.columns:
+                if feature in NaNRemovedData.columns: # NaN의 limit을 넘은 컬럼 삭제 후, 컬럼이 남아있으면
                     NaNRemovedData_feature = NaNRemovedData[[feature]]
                     finalFlag[feature] = 1
                     MDP = data_preprocessing.DataPreprocessing()
                     ImputedData[feature] = MDP.get_imputedData(NaNRemovedData_feature, self.imputation_param)
             else:
                 finalFlag[feature] = -1
+        
         return refinedData, NaNRemovedData, ImputedData, finalFlag
 
     
@@ -145,13 +161,14 @@ class CleanFeatureData:
         return refined_data, datawithMoreUnCertainNaN
 
         
+    # self.query_start_time, self.query_end_time 문제 이슈
     def _getDataWithFullDuration(self, data):
         # Make Data with Full Duration [query_start_time ~ query_end_time]
         if len(data)>0:
             #2. Make Full Data(query Start ~ end) with NaN
-            data.index = data.index.tz_localize(None)
+            data.index = data.index.tz_localize(None) # 지역정보 없이 시간대만 가져오기
             new_idx = pd.date_range(start = self.query_start_time, end = (self.query_end_time- self.resample_freq), freq = self.resample_freq)
             new_data = pd.DataFrame(index= new_idx)
             new_data.index.name ='time' 
-            data = new_data.join(data) 
+            data = new_data.join(data) # new_data에 data를 덮어쓴다
         return data
--- dataRemovebyNaN/data_remove_byNaN.py
+++ dataRemovebyNaN/data_remove_byNaN.py
@@ -7,24 +7,24 @@ class DataRemoveByNaNStatus():
     def __init__(self):
         pass
     ## Select one remove method : (time, ratio, num)
-    def removeNaNData(self, data, NanInfoForClenData):
-        type = NanInfoForClenData['type']
+    def removeNaNData(self, data, NanInfoForCleanData):
+        type = NanInfoForCleanData['type']
         # 0,0 일 경우 NaN 이 하나 이상일 경우 모두 삭제
         if type=='time':
             #ConsecutiveNanLimitTime_second =   60 *60
             #totalNaNLimitTime_second =  1 * 60
-            ConsecutiveNanLimitTime_second = NanInfoForClenData['ConsecutiveNanLimit']
-            totalNaNLimitTime_second =NanInfoForClenData['totalNaNLimit']
+            ConsecutiveNanLimitTime_second = NanInfoForCleanData['ConsecutiveNanLimit']
+            totalNaNLimitTime_second =NanInfoForCleanData['totalNaNLimit']
             data = self.removeNaNDataByTime(data, ConsecutiveNanLimitTime_second, totalNaNLimitTime_second)
             
         elif type =='num':
-            ConsecutiveNanLimitNum = NanInfoForClenData['ConsecutiveNanLimit']
-            totalNanLimitNum =NanInfoForClenData['totalNaNLimit']
+            ConsecutiveNanLimitNum = NanInfoForCleanData['ConsecutiveNanLimit']
+            totalNanLimitNum =NanInfoForCleanData['totalNaNLimit']
             data = self.removeNaNDataByNum(data, ConsecutiveNanLimitNum, totalNanLimitNum)
             
         elif type=='ratio':
-            ConsecutiveNanLimitNum = NanInfoForClenData['ConsecutiveNanLimit']
-            totalNanLimitNum =NanInfoForClenData['totalNaNLimit']
+            ConsecutiveNanLimitNum = NanInfoForCleanData['ConsecutiveNanLimit']
+            totalNanLimitNum =NanInfoForCleanData['totalNaNLimit']
             data = self.removeNaNDataByRatio(data, ConsecutiveNanLimitNum, totalNanLimitNum)    
         return data
     
