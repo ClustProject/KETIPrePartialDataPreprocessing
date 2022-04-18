6bebeef62eaebb80dbd3e1b5df3b0c82bca2fd03
jwmoonP <jaewonmoon.keti@gmail.com>
2022-04-18 12:44:41 +0900

    update

--- error_detection/dataOutlier.py
+++ error_detection/dataOutlier.py
@@ -20,15 +20,42 @@ class DataOutlier():
         :type raw_data: dataframe
         
         example
-            >>> config = { 
-                    'algorithm': 'IF', # outlier detection에 활용할 알고리즘 정의, {'SR', 'LOF', 'MoG', 'KDE', 'IF'} 중 택 1            
-                    'alg_parameter': {
-                        'percentile': 95, # 예측시 활용되는 outlier 임계값, int or float
+            >>> AlgParameterEx = {
+                    "IF":{
+                        'percentile': percentile, # 예측시 활용되는 outlier 임계값, int or float
                         'IF_estimators': 100, # ensemble에 활용하는 모델 개수, int(default: 100, 데이터 크기에 적합하게 설정)
                         'IF_max_samples': 'auto', # 각 모델에 사용하는 샘플 개수(샘플링 적용), int or float(default: 'auto')
                         'IF_contamination': 'auto', # 모델 학습시 활용되는 데이터의 outlier 비율, ‘auto’ or float(default: ’auto’, float인 경우 0 초과, 0.5 이하로 설정)
                         'IF_max_features': 1.0, # 각 모델에 사용하는 변수 개수(샘플링 적용), int or float(default: 1.0)
-                        'IF_bootstrap': False}, # bootstrap적용 여부, bool(default: False)           
+                        'IF_bootstrap': False}, # bootstrap적용 여부, bool(default: False)
+                    "KDE":{
+                        'percentile': percentile, # 예측시 활용되는 outlier 임계값, int or float
+                        'KDE_bandwidth': 0.2, # kernel의 대역폭, float(default: 1.0)
+                        'KDE_algorithm': 'auto', # 사용할 tree 알고리즘, {‘kd_tree’,‘ball_tree’,‘auto’}(default: ’auto’) 중 택 1
+                        'KDE_kernel': 'gaussian', # kernel 종류, {'gaussian’, ‘tophat’, ‘epanechnikov’, ‘exponential’, ‘linear’, ‘cosine’}(default: ’gaussian’) 중 택 1
+                        'KDE_metric': 'euclidean', # 사용할 거리 척도, str(default: ’euclidean’)
+                        'KDE_breadth_first': True, # breadth(너비) / depth(깊이) 중 우선순위 방식 정의, bool, True: breadth or False: depth
+                        'KDE_leaf_size': 40}, # tree 알고리즘에서의 leaf node 개수, int(default: 40)}
+                    "LOF":{
+                        'percentile': percentile, # 예측시 활용되는 outlier 임계값, int or float
+                        'LOF_neighbors': 5, # 가까운 이웃 개수, int(default: 20)
+                        'LOF_algorithm': 'auto', # 가까운 이웃을 정의하기 위한 알고리즘, {‘auto’, ‘ball_tree’, ‘kd_tree’, ‘brute’}(default: ’auto’) 중 택 1
+                        'LOF_leaf_size': 30, # tree 알고리즘에서의 leaf node 개수, int(default: 30)
+                        'LOF_metric': 'minkowski'}, # 이웃을 정의하기 위한 거리 척도, str or callable(default: ’minkowski’)
+                    "MoG": {# EM 방법론 반복 횟수, int(default: 100)
+                        'percentile': percentile, # 예측시 활용되는 outlier 임계값, int or float
+                        'MoG_components': 2, # mixture에 활용하는 component의 개수, int(default: 1)
+                        'MoG_covariance': 'full', # {‘full’, ‘tied’, ‘diag’, ‘spherical’}(default: ’full’) 중 택 1
+                        'MoG_max_iter': 100},
+                    "SR":{
+                        'percentile': percentile, # 예측시 활용되는 outlier 임계값, int or float
+                        'SR_series_window_size': 24, # series window 크기, int, 데이터 크기에 적합하게 설정
+                        'SR_spectral_window_size': 24, # spectral window 크기, int, 데이터 크기에 적합하게 설정
+                        'SR_score_window_size': 100}# score window 크기, int, period보다 충분히 큰 size로 설정
+                    }
+            >>> config = { 
+                    'algorithm': 'IF', # outlier detection에 활용할 알고리즘 정의, {'SR', 'LOF', 'MoG', 'KDE', 'IF'} 중 택 1            
+                    'alg_parameter': AlgParameterEx['IF']      
                 }
             >>> data_outlier = mod.DataOutlier(config, raw_data)
             >>> replaced_data, index_list = data_outlier.getResult()
@@ -39,7 +66,6 @@ class DataOutlier():
         self.data = raw_data.copy()
         
     ### Outlier Detector
-
     def getResult(self):
         """    
         :return index_list: indices of detected outliers
